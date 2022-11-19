2ab3c478127aae25568c5c04737547fa16532a20
RakhmatilloRR <rrr.engineer.94@gmail.com>
2022-11-19 14:51:28 +0500

    test

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const quizNumber = localStorage.getItem('quizNumber');
 const quiz = quizAll.filter((w) => {
   return (
     w.bookNumber === bookNumber &&
-    (w.quizNumber === +quizNumber || quizNumber.toString() === '0')
+    (w.quizNumber === +quizNumber || quizNumber.toString() === 'All')
   );
 });
 
